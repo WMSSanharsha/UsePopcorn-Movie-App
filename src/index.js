c4f9fc0f0acfd6426da7cdb111a7a2980aca6b36
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-10-06 13:20:13 +0530

    add stars

--- src/index.js
+++ src/index.js
@@ -1,10 +1,27 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
+// import "./index.css";
+// import App from "./App";
+import StarRating from "./StarRating";
+
+function Test() {
+  return <StarRating color="blue" />;
+}
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <StarRating maxRating={10} />
+    <StarRating maxRating={5} color="red" size={30} />
+    <StarRating
+      maxRating={5}
+      color="blue"
+      size={30}
+      messages={["Terrible", "Bad", "Average", "Good", "Excellent"]}
+    />
+    <StarRating maxRating={15} color="green" size={30} defaultRating={10} />
+
+    <Test />
   </React.StrictMode>
 );
